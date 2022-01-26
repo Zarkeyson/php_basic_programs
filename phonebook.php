169616d90a5e9511dc4304130d1962243184d001
Zarkeyson <djurdjev.zarko@gmail.com>
2022-01-26 12:06:58 +0100

    added voucher for number reveal

--- phonebook.php
+++ phonebook.php
@@ -1,21 +1,24 @@
 <?php
+  // Comment: status == 1 GOST, ako je status == 2 REGISTROVAN KORISNIK
+  $status = 1;
+  $vaucer_id = "123";
   $users = array(
     array(
       'name' => 'Petar',
       'prezime' => 'Petrovic',
-      'number' => '063513313'),
+      'number' => "063 555 666"),
     array(
       'name' => 'Nikola',
       'prezime' => 'Saravolac',
-      'number' => '063520521'),
+      'number' => "063 520 521"),
     array(
       'name' => 'Zarko',
       'prezime' => 'Djurdjev',
-      'number' => '063525320'),
+      'number' => "063 525 320"),
       array(
         'name' => 'Petar',
         'prezime' => 'Nikodijevic',
-        'number' => '063513616')
+        'number' => "063 513 616")
   );
  ?>
 <!DOCTYPE html>
@@ -27,26 +30,56 @@
   <body>
     <form action="phonebook.php" method="get">
       <input type="text" name="name" placeholder="Kriterijum...."/>
+      <input type="text" name="vaucer" placeholder="Vaucer...">
       <input type="submit" name="submit" value="Search"><br>
     </form>
       <hr>
       <?php
+
       if (isset($_GET['name'])) {   #isset funk da bismo sprecili sliku greske kad se prvi put udje na stranicu
+        if (isset($_GET['vaucer'])) {
+          $vaucer = $_GET['vaucer'];
+        }else {
+          $vaucer = "";
+        }
+
         $name = $_GET['name'];
         $name = strtolower($name);
         $brRezultata = 0;
         for ($i=0; $i < count($users); $i++) {
           $pripremljenoIme = strtolower($users[$i]['name']);
-          if ($name == $pripremljenoIme ) {
+          $pripremljenoPrezime = strtolower($users[$i]['prezime']);
+          if ($name == $pripremljenoIme || $name == $pripremljenoPrezime) {
             $brRezultata++;
             echo "Ime: " . $users[$i]['name'] . "<br>";
             echo "Prezime: " . $users[$i]['prezime'] . "<br>";
-            echo "Broj telefona: " . $users[$i]['number'] . "<br><hr>";
+            if (($status == 2) || ($vaucer == $vaucer_id)) {
+              echo "Broj telefona: " . $users[$i]['number'] . "<br><hr>";
+            }else {
+              echo "Broj telefona: ";
+              for ($j=0; $j<strlen($users[$i]['number']); $j+2) {
+                if ($j < 3) {
+                  echo $users[$i]['number'][$j];
+                }else {
+                  if (!($j%3)) {
+                    echo $users[$i]['number'][$j];
+                  }else {
+                    echo "*";
+                  }
+                }
+              }
+              echo "<br><hr>";
+            }
           }
-        }if ($brRezultata == 0) {
-          echo "Nema rezultata pretrage...";
+        }if ($brRezultata) {
+          echo "Broj rezultata pretrage za kriterijum <b>" . $_GET['name'] . "</b> je: " . $brRezultata . "<br>";
+          if ($vaucer == $vaucer_id) {
+            echo "<p style='color:red'>Pretraga uspesno izvrsena uz pomoc vaucera</p>";
+          }else {
+            echo "Da biste prikazali broj telefon molimo Vas unesite vazci vaucer";
+          }
         }else {
-          echo "Broj rezultata pretrage za kriterijum <b>" . $_GET['name'] . "</b> je: " . $brRezultata;
+          echo "Nema rezultata pretrage...";
         }
         }
       else {
